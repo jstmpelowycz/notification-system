d2ad1f5b76edcaf30e79b492ddefcd192ccbbe4e
Dmytro Pelovych <dmytro.pelovych@howly.com>
2025-04-28 22:42:01 +0300

    Set up env typing

---
+++ backend/src/types/environment.d.ts
@@ -0,0 +1,16 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'development' | 'production' | 'test';
+            PORT: string;
+
+            DB_HOST: string;
+            DB_PORT: string;
+            DB_USERNAME: string;
+            DB_PASSWORD: string;
+            DB_NAME: string;
+        }
+    }
+}
+
+export {};
