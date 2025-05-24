fc3be5578df1f43c1367155a17cc8b7a39a2210f
Dmytro Pelovych <dmytro.pelovych@howly.com>
2025-05-24 12:51:20 +0300

    Ensure better validation

--- backend/src/entities/notification-channel/validation/config-validation-rule.ts
+++ backend/src/entities/notification-channel/validation/config-validation-rule.ts
@@ -1,9 +1,16 @@
+
+
+
+
 interface NotificationChannelConfigValidationRule {
     validate: (config: Record<string, unknown>) => boolean;
     errorMessage: string;
 }
 
 export const WEBHOOK_PROVIDER_INTEGRATION_TYPE_CONFIG_VALIDATION_RULE: NotificationChannelConfigValidationRule = {
-    validate: config => 'webhookUrl' in config,
+    validate: config => (
+        'webhookUrl' in config
+        && typeof config.webhookUrl === 'string'
+    ),
     errorMessage: 'Config must be defined with "webhookUrl"',
 };
